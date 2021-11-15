959aea179a43e2824c7b9d630101e9c90b6c5c6c
Ayman El-Ghoneimy <ayman.gaafer@gmail.com>
2021-11-15 16:09:06 +0000

    committing the bug

---
+++ treeview-multiselect-bug/MainPage.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.ObjectModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using muxc = Microsoft.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace treeview_multiselect_bug
+{
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Item> DataSource = new ObservableCollection<Item>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DataSource = GetDessertData();
+        }
+
+        private ObservableCollection<Item> GetDessertData()
+        {
+            var list = new ObservableCollection<Item>();
+
+            Item flavorsCategory = new Item()
+            {
+                Name = "Flavors",
+                Children =
+                {
+                    new Item() { Name = "Vanilla" },
+                    new Item() { Name = "Strawberry" },
+                    new Item() { Name = "Chocolate" }
+                }
+            };
+
+            Item toppingsCategory = new Item()
+            {
+                Name = "Toppings",
+                Children =
+                {
+                    new Item()
+                    {
+                        Name = "Candy",
+                        Children =
+                        {
+                            new Item() { Name = "Chocolate" },
+                            new Item() { Name = "Mint" },
+                            new Item() { Name = "Sprinkles" }
+                        }
+                    },
+                    new Item()
+                    {
+                        Name = "Fruits",
+                        Children =
+                        {
+                            new Item() { Name = "Mango" },
+                            new Item() { Name = "Peach" },
+                            new Item() { Name = "Kiwi" }
+                        }
+                    },
+                    new Item()
+                    {
+                        Name = "Berries",
+                        Children =
+                        {
+                            new Item() { Name = "Strawberry" },
+                            new Item() { Name = "Blueberry" },
+                            new Item() { Name = "Blackberry" }
+                        }
+                    }
+                }
+            };
+
+            list.Add(flavorsCategory);
+            list.Add(toppingsCategory);
+            return list;
+        }
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            FlavorList.Text = string.Empty;
+            ToppingList.Text = string.Empty;
+
+            foreach (muxc.TreeViewNode node in DessertTree.SelectedNodes)
+            {
+                if (node.Parent.Content?.ToString() == "Flavors")
+                {
+                    FlavorList.Text += node.Content + "; ";
+                }
+                else if (node.HasChildren == false)
+                {
+                    ToppingList.Text += node.Content + "; ";
+                }
+            }
+        }
+
+        private void SelectAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (DessertTree.SelectionMode == muxc.TreeViewSelectionMode.Multiple)
+            {
+                DessertTree.SelectAll();
+            }
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public ObservableCollection<Item> Children { get; set; } = new ObservableCollection<Item>();
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
